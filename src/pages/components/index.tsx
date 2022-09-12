4870bec72e656e5b06a068b38d9283546df0c1da
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-09-12 18:49:14 -0300

    c

--- src/pages/components/index.tsx
+++ src/pages/components/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { HeroQuestionStyle } from "./style"
 
 type Props = {
@@ -11,16 +12,25 @@ type Props = {
 }
 
 export const HerosQuestions = ({question01,parameter01,question02,parameter02,question03,parameter03}:Props) => {
+    const [questionOne, setQuestionOne] = useState<string>()
+    const [questionTwo, setQuestionTwo] = useState<string>()
+    const [questionThree, setQuestionThree] = useState<string>()
+
+    const handleButton = () => {
+        if(questionOne === '' || questionTwo === ''|| questionThree ==='') return alert('Todos os campos devem estar preenchidos')
+        
+    }
+
     return (
         <HeroQuestionStyle>
             <div id="container">
                 <p>{question01}</p>
-                <input type="text" name="" id="" placeholder="digite aqui"/>
+                <input type="text" name="" id="" placeholder="digite aqui" onChange={(event)=>setQuestionOne(event.target.value)}/>
                 <p>{question02}</p>
-                <input type="text" name="" id="" placeholder="digite aqui"/>
+                <input type="text" name="" id="" placeholder="digite aqui" onChange={(event)=>setQuestionTwo(event.target.value)}/>
                 <p>{question03}</p>
-                <input type="text" name="" id="" placeholder="digite aqui"/>
-                <button>Proxima página</button>
+                <input type="text" name="" id="" placeholder="digite aqui" onChange={(event)=>setQuestionThree(event.target.value)}/>
+                <button onClick={handleButton}>Proxima página</button>
             </div>      
         </HeroQuestionStyle>
     )
